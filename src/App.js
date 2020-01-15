f6f2bcec6b2427d951c1e2ebd2c802af3e5f49c4
Yonatan Melka <ybmelka2@gmail.com>
2020-01-15 13:52:48 -0600

    Add component files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
+import SearchForm from './components/SearchForm'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
           <Container>
             <h1>Search App</h1>
             <p>This is a simple search app</p>
+            <SearchForm />
           </Container>
         </Jumbotron>
       </div>
