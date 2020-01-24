5c801c7bece90f60b2c339ea83352f02af4a73a7
Yonatan Melka <ybmelka2@gmail.com>
2020-01-24 14:59:48 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-import { Jumbotron, Container, Row, Col } from 'react-bootstrap';
+import { Jumbotron, Container, Row, Col, Button } from 'react-bootstrap';
 import SearchForm from './components/SearchForm'
 import Results from './components/Results'
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      count: 1
+    }
+  }
+
+  addListItem = () => {
+    const listGroupItem = document.createElement('div');
+    listGroupItem.className = 'list-group-item';
+    const listItem = document.getElementById('list-item');
+    listGroupItem.innerText = 'New list item';
+    listItem.appendChild(listGroupItem);
+  }
+
   render() {
     return (
       <div>
@@ -16,6 +32,16 @@ class App extends Component {
               </Col>
               <Col>
                 <SearchForm />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <h1>Click on the list</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Button onClick={this.addListItem}> Add list item </Button>
               </Col>
             </Row>
           </Container>
--- src/components/Results.js
+++ src/components/Results.js
@@ -10,10 +10,11 @@ const Results = () => {
   return (
     <Container>
       <h2>Result List</h2>
-      <ListGroup>
-        <ListGroupItem href="#" active>Link 1</ListGroupItem>
-        <ListGroupItem href="#">Link 2</ListGroupItem>
-        <ListGroupItem href="#" disabled>Link 3</ListGroupItem>
+      <ListGroup id="list-item">
+        <ListGroupItem href="#" key={1}>Link 1</ListGroupItem>
+        <ListGroupItem href="#"key={2}>Link 2</ListGroupItem>
+        <ListGroupItem href="#" key={3}>Link 3</ListGroupItem>
+        <div className="list-group-item">Link 4</div>
       </ListGroup>
     </Container>
   )
